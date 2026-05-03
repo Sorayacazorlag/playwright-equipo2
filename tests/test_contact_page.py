65f7eae757f286e877ef00af3587b5ab0777d295
Teresa Mercader <teresam0510@gmail.com>
2026-05-03 10:32:40 +0200

    submit form email field left empty
    Probando segundo test automatizado de contacto con email requerido vacio

--- tests/test_contact_page.py
+++ tests/test_contact_page.py
@@ -16,7 +16,7 @@ def test_complete_and_submit_the_contact_form_with_mandatory_fields(page: Page):
 
 
 def test_form_with_required_name_field_left_empty(page: Page):
-    print("Given the users enters contact page 'Contact| Vida Verde ")
+    print("Given the users enters contact page 'Contact| Vida Verde'")
     page.goto("https://web-qa.dev.adalab.es/contact")
 
     print ("fills required email with 'test@gmail.com'")
@@ -32,6 +32,27 @@ def test_form_with_required_name_field_left_empty(page: Page):
     print ("user should see the error message 'name is mandatory'")
     expect(page.get_by_text("El nombre es obligatorio")).to_be_visible()
 
+
+from playwright.sync_api import Page, expect
+
+def test_form_with_required_email_field_left_empty(page: Page):
+
+    print ("given the users contact page 'Contact| Vida Verde'")
+    page.goto("https://web-qa.dev.adalab.es/contact")
+    
+    print ("fills in the required name with 'Ana Sanchez'")
+    page.get_by_role("textbox", name="Nombre *").fill("Ana Sanchez")
+
+    print ("fills in the required message field with 'test message'")
+    page.get_by_role("textbox", name="Mensaje *").fill("text message")
+
+    print ("cliks send")
+    page.get_by_role("button", name="Enviar Mensaje").click()
+
+    print ("users should see the message 'message sent succesufully'")
+    page.get_by_role("heading", name="¡Mensaje enviado con éxito!").click()
+    expect(page.get_by_role("heading", name="¡Mensaje enviado con éxito!")).to_be_visible()
+    
 
 
 
