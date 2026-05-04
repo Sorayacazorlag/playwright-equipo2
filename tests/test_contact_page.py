8fbca44c5ce0567b56597e90d01c04d4499d52b0
Teresa Mercader <teresam0510@gmail.com>
2026-05-04 23:42:56 +0200

    Update test_contact_page.py
    Añadir pagina web page.goto

--- tests/test_contact_page.py
+++ tests/test_contact_page.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Page, expect
 
 def test_complete_and_submit_the_contact_form_with_mandatory_fields(page: Page):
     print("Given la usuaria abre la página de contacto 'Contáctanos | Vida Verde'")
-    
+    page.goto("https://web-qa.dev.adalab.es/contact")
     print("When rellena el nombre")
     page.get_by_role("textbox", name="Nombre *").fill("Marta Diaz")
     print("And rellena el email")
