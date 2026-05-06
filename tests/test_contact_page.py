79f2eddcd41553dada51d6a71382334691bea85b
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-05-06 15:35:30 +0200

    Update test_contact_page.py

--- tests/test_contact_page.py
+++ tests/test_contact_page.py
@@ -67,27 +67,6 @@ def test_submit_form_empty_required_message(page: Page):
     print("Then they should see an error message: El mensaje es obligatorio")
     expect(page.get_by_text("El mensaje es obligatorio")).to_be_visible()
 
-def submit_form_invalid_required_email(page: Page):
-    
-    print("Given the user is on the page: Contáctenos | Vida Verde")
-    page.goto("https://web-qa.dev.adalab.es/contact")
-    
-    print("When they fill in the required name field")
-    page.get_by_role("textbox", name="Nombre *").fill("Marta Díaz")
-    
-    print("And they fill in the required email field with an invalid email")
-    page.get_by_role("textbox", name="Email *").fill("email")
-    
-    print("And they fill in the required message field")
-    page.get_by_role("textbox", name="Mensaje *").fill("test mensaje")
-    
-    print("And they click on submit")
-    page.get_by_role("button", name="Enviar Mensaje").click()
-
-    print("Then they should see an error message")
-    expect(page.get_by_text("El formato del email no es vá")).to_be_visible()
-
-
 
     
 
