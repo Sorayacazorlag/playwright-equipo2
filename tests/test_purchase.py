c03c76e654f66bb13035ccad8d286e034a1324ad
Teresa Mercader <teresam0510@gmail.com>
2026-05-09 22:12:36 +0200

    Update test_purchase.py
    test data valid

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -15,6 +15,7 @@ def test_successful_purchase_with_valid_data(page: Page):
 
     print("checkout purchase")
     page.get_by_role("link", name="Finalizar Compra").click()
+    expect(page.get_by_text("Maceta Colgante")).to_be_visible()
 
     print("click on proceed to payment")
     page.get_by_role("link", name="Proceder al Pago").click()
@@ -24,12 +25,18 @@ def test_successful_purchase_with_valid_data(page: Page):
     page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("4242 4242 4242 4242")
     page.get_by_role("button", name="Completar Compra").click()
 
+    # Verificar página de confirmación
     expect(page.get_by_role("heading", name="¡Compra Realizada con Éxito!")).to_be_visible()
+    expect(page.get_by_text("Maceta Colgante")).to_be_visible()
+    expect(page.locator("data.text-gray-900").nth(0)).to_be_visible()  # Total : 14.75
+    expect(page.locator("data.text-gray-900").nth(1)).to_be_visible()  # Subtotal: 14.75
+    expect(page.locator("data.text-gray-900").nth(2)).to_be_visible()  # VAT: 3.10
+    expect(page.locator("data.text-gray-900").nth(3)).to_be_visible()  # Shipping: 5.00
+    expect(page.locator("data.text-green-600")).to_be_visible()        # Total: 22.85
+
+
     page.get_by_role("link", name="Volver a la Tienda").click()
-    expect(page.get_by_role("heading", name="Catálogo de Productos")).to_be_visible()
-
-
-
+    expect(page).to_have_url("https://web-qa.dev.adalab.es/products")
 
 
 
