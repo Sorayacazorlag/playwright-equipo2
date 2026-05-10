02069b382b8307ca6fbb59567142ec917b3af712
Teresa Mercader <teresam0510@gmail.com>
2026-05-10 12:59:50 +0200

    Update test_visit_page.py

--- tests/test_visit_page.py
+++ tests/test_visit_page.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page, expect
 
 def test_visit(page: Page):
     print("Given user visit homepage")
-    page.goto("https://bootcampqa.com")
-    
+    page.goto("https://web-qa.dev.adalab.es")
+    page.pause()
+
 
