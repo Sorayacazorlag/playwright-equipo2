99ec8210d28c3bc71e62b63cd3b4feea1c3670c3
Teresa Mercader <teresam0510@gmail.com>
2026-05-10 18:30:00 +0200

    Update test_contact_page.py
    refactorizando test  empty email

--- tests/test_contact_page.py
+++ tests/test_contact_page.py
@@ -6,7 +6,7 @@ def test_complete_and_submit_the_contact_form_with_mandatory_fields(page: Page):
     contact_page = ContactPage(page)
 
     print("Given la usuaria abre la página de contacto 'Contáctanos | Vida Verde'")
-    page.goto("https://web-qa.dev.adalab.es/contact")
+    self.page.goto(self.url)
 
     print("When rellena el nombre")
     page.get_by_role("textbox", name="Nombre *").fill("Marta Diaz")
@@ -27,20 +27,19 @@ def test_complete_and_submit_the_contact_form_with_mandatory_fields(page: Page):
 
 def test_form_with_required_name_field_left_empty(page: Page):
     print("Given the users enters contact page 'Contact| Vida Verde'")
-    page.goto("https://web-qa.dev.adalab.es/contact")
+    self.page.goto(self.url)
 
     print ("fills required email with 'test@gmail.com'")
-    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+    self.page.get_by_role("textbox", name="Email *").fill(email)
    
     print ("fills required message with 'test mesage'")
-    page.get_by_role("textbox", name="Mensaje *").fill("test message")
-    page.get_by_role("textbox", name="Nombre *").click()
+    self.page.get_by_role("textbox", name="Mensaje *").fill(mensaje)
 
     print ("clicks send")
-    page.get_by_role("button", name="Enviar Mensaje").click()
+    self.page.get_by_role("button", name="Enviar Mensaje").click()
 
     print ("user should see the error message 'name is mandatory'")
-    expect(page.get_by_text("El nombre es obligatorio")).to_be_visible()
+    expect(self.page.get_by_text(text)).to_be_visible()
 
 
 from playwright.sync_api import Page, expect
