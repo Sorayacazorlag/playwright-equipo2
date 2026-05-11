62ae85f1eda7caf49a32349d76c1bbb45e9eafdd
Cristina Hernandez <crispallares7500@gmail.com>
2026-05-11 19:10:08 +0200

    Update product_filter.py

--- pages/product_filter.py
+++ pages/product_filter.py
@@ -1,19 +1,31 @@
 from playwright.sync_api import Page, expect
 
+class ProductFilterPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.url = "https://web-qa.dev.adalab.es/products"
+        self.title = "Catálogo de Productos"
 
 
+    def open_products_page(self):
+        self.page.goto(self.url)
 
-def Filter_by_valid_name_price_and_category_values(page: Page):
-    page.goto("https://web-qa.dev.adalab.es/products")
-    page.get_by_role("searchbox", name="Nombre").fill("regadera")
-    page.get_by_label("CategoríaTodas las categorí").select_option("Herramientas")
-    page.get_by_role("spinbutton", name="Precio mínimo").fill("20")
-    page.get_by_role("spinbutton", name="Precio máximo").fill("25")
-    expect(page.get_by_role("region", name="Catálogo de productos").get_by_role("list")).to_be_visible()
+        
+
+    def filter_by_valid_name_price_and_category_values(self):
+        self.page.goto("https://web-qa.dev.adalab.es/products")
+        self.page.get_by_role("searchbox", name="Nombre").fill("regadera")
+        self.page.get_by_label("CategoríaTodas las categorí").select_option("Herramientas")
+        self.page.get_by_role("spinbutton", name="Precio mínimo").fill("20")
+        self.page.get_by_role("spinbutton", name="Precio máximo").fill("25")
+        expect(self.page.get_by_role("region", name="Catálogo de productos").get_by_role("list")).to_be_visible()
 
 
-
-def Filter_by_a_value_with_no_results(page: Page):
-    page.goto("https://web-qa.dev.adalab.es/products")
-    page.get_by_role("searchbox", name="Nombre").fill("manzana")
-    expect(page.get_by_text("No se encontraron productos")).to_be_visible()
+    def filter_by_a_value_with_no_results(self):
+        self.page.goto("https://web-qa.dev.adalab.es/products")
+        self.page.get_by_role("searchbox", name="Nombre").fill("manzana")
+        expect(self.page.get_by_text("No se encontraron productos")).to_be_visible()    
+def Filter_by_a_value_with_no_results(self):
+    self.page.goto("https://web-qa.dev.adalab.es/products")
+    self.page.get_by_role("searchbox", name="Nombre").fill("manzana")
+    expect(self.page.get_by_text("No se encontraron productos")).to_be_visible()
