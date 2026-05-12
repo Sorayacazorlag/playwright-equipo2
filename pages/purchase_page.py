b4690ab6be2f081333b9a0a3a08ae0d9291fd1a7
Teresa Mercader <teresam0510@gmail.com>
2026-05-13 00:30:16 +0200

    purchase with invalid data

--- pages/purchase_page.py
+++ pages/purchase_page.py
@@ -2,7 +2,6 @@
 from playwright.sync_api import Page, expect
 
 
-
 class PurchasePage:
 
     def __init__(self, page: Page):
@@ -28,22 +27,99 @@ class PurchasePage:
     def checkout_purchase(self,name):
         expect(self.page.get_by_text(name)).to_be_visible()
 
-    def press_send_payment (self,text)
+    def press_send_payment (self,text):
         self.page.get_by_role("link", name=text).click()
 
-    def fill_complete_name (self,name)
+    def fill_complete_name (self,name):
         self.page.get_by_role("textbox", name="Nombre" *"").fill(name)
 
-    def fill_email(self,email)
+    def fill_email(self,email):
         self.page.get_by_role("textbox", name="Email *").fill(email)
 
-    def fill_adress(self,adress)
+    def fill_adress(self,adress):
         self.page.get_by_role("textbox", name="direccion").fill(adress)
 
-    def fill_credit_card(self, credit_card)
+    def fill_credit_card(self, credit_card):
         self.page.get_by_role("textbox", name= "tarjeta credito").fill(credit_card)
 
-    def press_place_older(self,text)
+    def press_place_older(self,text):
         self.page.get_by_role("button", name="Completar Compra").click(text)
 
-       
+    def verify_message_form(self,text):
+        expect(self.page.get_by_role("heading", name= text)).to_be_visible()  
+
+    def verify_see_message(self,name):
+        expect(self.page.get_by_text(name)).to_be_visible()
+
+    def verify_product_price(self, price):
+        expect(self.page.locator("data.text-gray-900").nth(0)).to_have_text(price)
+
+    def verify_subtotal(self, subtotal):
+        expect(self.page.locator("data.text-gray-900").nth(1)).to_have_text(subtotal)
+
+    def verify_vat(self, vat):
+        expect(self.page.locator("data.text-gray-900").nth(2)).to_have_text(vat)
+
+    def verify_shipping(self, shipping):
+        expect(self.page.locator("data.text-gray-900").nth(3)).to_have_text(shipping)
+
+    def verify_total(self, total):
+        expect(self.page.locator("data.text-green-600")).to_have_text(total)
+
+    def back_shopping(self,text):
+        self.page.get_by_role("link", name=text).click()
+   
+    def verify_open_website(self,url):
+        expect(self.page).to_have_url(url)
+
+def test_unsuccessful_purchase_with_empty_credit_card_field (page: Page):
+   
+   from playwright.sync_api import Page, expect
+
+   class PurchasePage:
+
+
+    def __init__(self, page: Page):
+        self.page = page
+        self.url = "https://web-qa.dev.adalab.es/products"
+        self.title = "Catálogo de productos"
+
+    def open_purchase_page(self):
+        self.page.goto(self.url)
+        
+    def filter_product_name(self,name):
+        self.page.get_by_role("searchbox", name="Nombre").fill(name) 
+
+    def type_of_product(self,text):
+      self.page.get_by_role("heading", name= text).click()
+
+    def add_to_cart(self,text):
+        self.page.get_by_role("button", name=text).click()
+
+    def checkout_purchase(self,name):
+        expect(self.page.get_by_text(name)).to_be_visible()
+
+    def press_send_payment (self,text):
+        self.page.get_by_role("link", name=text).click()
+
+    def fill_complete_name (self,name):
+        self.page.get_by_role("textbox", name="Nombre" *"").fill(name)
+
+    def fill_email(self,email):
+        self.page.get_by_role("textbox", name="Email *").fill(email)
+
+    def fill_adress(self,adress):
+        self.page.get_by_role("textbox", name="direccion").fill(adress)
+
+    def fill_credit_card(self, credit_card):
+        self.page.get_by_role("textbox", name= "tarjeta credito").fill(credit_card)
+
+    def message_credit_card_invalid(self,text):
+        expect(self.page.get_by_text(text)).to_be_visible()
+
+
+
+
+    
+    expect(page.get_by_text("Tarjeta de crédito no válida.")).to_be_visible()
+    page.get_by_role("link", name="Volver al Carrito").click()
--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -30,7 +30,7 @@ def test_successful_purchase_with_valid_data(page: Page):
     purchase_page.fill_adress("calle parmenides,5 Malaga")
     purchase_page.fill_credit_card("4242 4242 4242 4242")
     purchase_page.press_place_older.click("completar compra")    
-    
+
 
     # Verificar página de confirmación
 
@@ -48,30 +48,36 @@ def test_successful_purchase_with_valid_data(page: Page):
     
 
 def test_unsuccessful_purchase_with_invalid_data(page: Page):
+
+    purchase_page = PurchasePage(page)
+
+
     print("given the users products page 'Products| Vida Verde'")
-    page.goto("https://web-qa.dev.adalab.es/products")
+    purchase_page.open_purchase_page("https://web-qa.dev.adalab.es/products")
 
     print("user name filter by 'maceta'")
-    page.get_by_role("searchbox", name="Nombre").fill("maceta")
+    purchase_page.filter_product_name("maceta")
 
     print("click type of 'maceta colgante'")
-    page.get_by_role("heading", name="Maceta Colgante").click()
+    purchase_page.type_of_product.click("maceta colgante")
 
     print("add to cart 'maceta colgante'")
-    page.get_by_role("button", name="Añadir Maceta Colgante al").click()
+    purchase_page.add_to_cart.click("maceta colgante")
 
     print("checkout purchase")
-    page.get_by_role("link", name="Finalizar Compra").click()
-    expect(page.get_by_text("Maceta Colgante")).to_be_visible()
+    purchase_page.press_send_checkout_purchase.click("finalizar compra")
+    purchase_page.checkout_purchase.click("maceta colgante")
 
     print("click on proceed to payment")
-    page.get_by_role("link", name="Proceder al Pago").click()
-    page.get_by_role("textbox", name="Nombre Completo *").fill("maria garcia")
-    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
-    page.get_by_role("textbox", name="Dirección *").fill("calle parmenides, 5 Malaga")
-    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("1111 4242 4242 4242 4242")
-    page.get_by_role("button", name="Completar Compra").click()
-    expect(page.get_by_text("Tarjeta de crédito no válida.")).to_be_visible()
+    purchase_page.press_send_payment.click("Proceder al Pago")
+    purchase_page.fill_complete_name.click("maria garcia")
+    purchase_page.fill_email.fill("test@gmail.com")
+    purchase_page.fill_adress("calle parmenides,5 Malaga")
+    purchase_page.fill_credit_card("4242 4242 4242 4242")
+    purchase_page.press_place_older.click("completar compra")
+    purchase_page.m
+ 
+
     page.get_by_role("link", name="Volver al Carrito").click()
 
 
