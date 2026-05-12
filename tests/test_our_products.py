99996183209c724f17c5b004510ea5983149decc
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-05-12 17:57:22 +0200

    Update test_our_products.py

--- tests/test_our_products.py
+++ tests/test_our_products.py
@@ -1,18 +1,21 @@
-from playwright.sync_api import Page, expect
- 
+from playwright.sync_api import Page
+from pages.products_page import ProductsPage
 
 
-url = 'https://web-qa.dev.adalab.es/products'
+def test_products_page(page: Page):
+    products_page = ProductsPage(page);
 
-def test_view_the_Our_Products_page(page: Page):
-    print("When el usuario abre la página de productos 'Nuestros Productos | Vida Verde'")
-    page.goto(url)
+    print("When the user enters to the products page")
+    products_page.open_products_page()
 
-    print("Then el usuario ve la categoría del producto 'Plantas'")
-    expect(page.get_by_text("Plantas").nth(2)).to_be_visible()
+    
+    print("Then the user should see the product category 'Plants'")
+    products_page.verify_products_category("Plantas")
+    
 
-    print("And el usuario ve el nombre del producto 'Ficus Lyrata'")
-    expect(page.get_by_role("heading", name="Ficus Lyrata")).to_be_visible()
+    print("And the user should see the product name 'Ficus Lyrata'")
+    products_page.verify_products_name("Ficus Lyrata")
 
-    print("And el usuario ve el precio del producto '35.00€'")
-    expect(page.get_by_text("35.00 €")).to_be_visible()
+
+    print("And the user should see the product price '35.00€")
+    products_page.verify_products_price("35.00 €")
