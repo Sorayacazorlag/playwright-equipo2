c5fa4ac0efabcb20a0b37bac407584b82051f0db
Cristina Hernandez <crispallares7500@gmail.com>
2026-05-15 18:40:08 +0200

    test_product_filter
    correccion

--- tests/test_product_filter.py
+++ tests/test_product_filter.py
@@ -1,16 +1,37 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+from pages.products_page import ProductsPage
 
 def Filter_by_valid_name_price_and_category_values(page: Page):
-    page.goto("https://web-qa.dev.adalab.es/products")
-    page.get_by_role("searchbox", name="Nombre").fill("regadera")
-    page.get_by_label("CategoríaTodas las categorí").select_option("Herramientas")
-    page.get_by_role("spinbutton", name="Precio mínimo").fill("20")
-    page.get_by_role("spinbutton", name="Precio máximo").fill("25")
-    expect(page.get_by_role("region", name="Catálogo de productos").get_by_role("list")).to_be_visible()
+     productos_page= ProductsPage(page)
+
+    print("Given: La usuaria abre la página de productos | Vida Verde")
+    productos_page.open_products_page()
+
+    print("When: La usuaria filtra por nombre 'Regadera'")
+    productos_page.filtrar_por_nombre("Regadera") 
+
+    print("And filtra por categoría 'Herramientas'")  
+    productos_page.filtrar_por_categoria("Herramientas")
+
+    print("And filtra por precio mínimo '20'")
+    productos_page.filtrar_por_precio_minimo("20")
+
+    print("And filtrar por precio máximo '25'")
+    productos_page.filtrar_por_precio_maximo("25")
+  
+    print("Then debe ver el producto 'Regadera Metálica'")
+    productos_page.verify_products_name("Regadera Metálica")
 
 
 
 def Filter_by_a_value_with_no_results(page: Page):
-    page.goto("https://web-qa.dev.adalab.es/products")
-    page.get_by_role("searchbox", name="Nombre").fill("manzana")
-    expect(page.get_by_text("No se encontraron productos"))
+    productos_page = ProductsPage(page)
+
+    print("Given la usuaria entra en la página de productos")
+    productos_page.open_products_page()
+
+    print("When filtra por el nombre 'manzana'")
+    productos_page.filtrar_por_nombre("manzana")
+
+    print("Then debería ver el mensaje 'No se encontraron productos'")
+    productos_page.verificar_mensaje_no_resultados()   
