949795ca490e1f6159bf106093b0a74573f79c6e
Teresa Mercader <teresam0510@gmail.com>
2026-05-15 21:58:34 +0200

    test_successful_purchase_with_valid_data
    test valido

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,4 +1,45 @@
 
+from playwright.sync_api import Page, expect
+
+def test_successful_purchase_with_valid_data(page: Page):
+    print("Given user visit products page")
+    page.goto("https://web-qa.dev.adalab.es/products")
+
+    print ("the user fills the valid field")
+    page.get_by_role("searchbox", name="Nombre").fill("maceta")
+
+    print ("the user choose maceta colgante")
+    page.get_by_role("heading", name="Maceta Colgante").click()
+    
+    print ("the user add maceta colgante to the cart")
+    page.get_by_role("button", name="Añadir Maceta Colgante al").click()
+
+    print ("the user visit the cart page")
+    page.get_by_role("link", name="Finalizar Compra").click()
+
+    print ("proceed to payment")
+    page.get_by_role("link", name="Proceder al Pago").click()
+
+    print ("fill complete name")
+    page.get_by_role("textbox", name="Nombre Completo *").fill("maria garcia")
+
+    print ("fill valid email user ")
+    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+
+    print("fill valid adress user")
+    page.get_by_role("textbox", name="Dirección *").fill("parmenides,5,Malaga")
+
+    print ("add valid credit card number")
+    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("4242 4242 4242 4242")
+
+    print ("complete purchse")
+    page.get_by_role("button", name="Completar Compra").click()
+    expect(page.get_by_role("heading", name="¡Compra Realizada con Éxito!")).to_be_visible()
+
+    print("back to products page")
+    page.get_by_role("link", name="Volver a la Tienda").click()
+
+    # ---------------------
 
 
 
