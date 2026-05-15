28975ee79ced120d5e6ede880694eca8f2ffdb4a
Teresa Mercader <teresam0510@gmail.com>
2026-05-15 22:15:17 +0200

    def test_successful_purchase_with_invalid_data(page: Page):          print ("Given user visit products page")     page.goto("https://web-qa.dev.adalab.es/products")      print ("the user fills the valid field")     page.get_by_role("searchbox", name="Nombre").fill("maceta")      print ("the user choose maceta colgante")     page.get_by_role("heading", name="Maceta Colgante").click()          print ("the user add maceta colgante to the cart")     page.get_by_role("button", name="Añadir Maceta Colgante al").click()      print ("the user visit the cart page")     page.get_by_role("link", name="Finalizar Compra").click()      print ("proceed to payment")     page.get_by_role("link", name="Proceder al Pago").click()      print ("fill complete name")     page.get_by_role("textbox", name="Nombre Completo *").fill("maria garcia")      print ("fill valid email user ")     page.get_by_role("textbox", name="Email *").fill("test@gmail.com")      print("fill valid adress user")     page.get_by_role("textbox", name="Dirección *").fill("parmenides,5,Malaga")      print ("fill invalid credit card number")     page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("1111 4242")      print("complete purchase")     page.get_by_role("button", name="Completar Compra").click()     expect(page.get_by_text("El número de tarjeta debe")).to_be_visible()
    VALIDO

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -40,6 +40,42 @@ def test_successful_purchase_with_valid_data(page: Page):
     page.get_by_role("link", name="Volver a la Tienda").click()
 
     # ---------------------
+def test_successful_purchase_with_invalid_data(page: Page):
+    
+    print ("Given user visit products page")
+    page.goto("https://web-qa.dev.adalab.es/products")
 
+    print ("the user fills the valid field")
+    page.get_by_role("searchbox", name="Nombre").fill("maceta")
 
+    print ("the user choose maceta colgante")
+    page.get_by_role("heading", name="Maceta Colgante").click()
+    
+    print ("the user add maceta colgante to the cart")
+    page.get_by_role("button", name="Añadir Maceta Colgante al").click()
+
+    print ("the user visit the cart page")
+    page.get_by_role("link", name="Finalizar Compra").click()
+
+    print ("proceed to payment")
+    page.get_by_role("link", name="Proceder al Pago").click()
+
+    print ("fill complete name")
+    page.get_by_role("textbox", name="Nombre Completo *").fill("maria garcia")
+
+    print ("fill valid email user ")
+    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+
+    print("fill valid adress user")
+    page.get_by_role("textbox", name="Dirección *").fill("parmenides,5,Malaga")
+
+    print ("fill invalid credit card number")
+    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("1111 4242")
+
+    print("complete purchase")
+    page.get_by_role("button", name="Completar Compra").click()
+    expect(page.get_by_text("El número de tarjeta debe")).to_be_visible()
+
+    print ("back to cart")
+    page.get_by_role("link", name="Volver al Carrito").click()
 
