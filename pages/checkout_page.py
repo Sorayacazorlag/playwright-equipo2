f811c4f7c5062741719c62132948eae2e2e2d6e7
Teresa Mercader <teresam0510@gmail.com>
2026-05-17 10:59:15 +0200

    .

---
+++ pages/checkout_page.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import Page, expect
+
+class CheckoutPage():
+
+    def __init__(self, page: Page):
+        self.page = page
+        self.url = "https://web-qa.dev.adalab.es/products"
+
+
+    def open_products_page(self):
+        self.page.goto(self.url)
+
