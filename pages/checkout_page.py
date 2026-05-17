b088b14ec217b5b558ba4a84e539ee8a4676b4d0
Teresa Mercader <teresam0510@gmail.com>
2026-05-17 12:44:41 +0200

    .

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,3 +1,4 @@
+import re
 from playwright.sync_api import Page, expect
 
 class CheckoutPage():
@@ -6,11 +7,10 @@ class CheckoutPage():
         self.page = page
         self.url = "https://web-qa.dev.adalab.es/products"
 
-
     def open_products_page(self):
         self.page.goto(self.url)
 
     def fill_valid_name(self,name):
         self.page.get_by_role("searchbox", name="Nombre").fill(name)
 
-        
+  
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -15,12 +15,15 @@ def test_successful_purchase_with_valid_data(page: Page): #Teresa
 
     print ("the user fills valid name")
     checkout_page.fill_valid_name("maceta")
-
+    
     print ("the user choose maceta colgante")
-    page.get_by_role("heading", name="Maceta Colgante").click()
+    checkout_page.add_product()
+
+    print ("verify maceta colgante")
+    expect(page.get_by_role("button", name="Añadir Maceta Colgante al")).to_be_visible()
     
     print ("the user add maceta colgante to the cart")
-    page.get_by_role("button", name="Añadir Maceta Colgante al").click()
+    page.get_by_role("button", name="añadir maceta colgante al").click()
 
     print ("the user visit the cart page")
     page.get_by_role("link", name="Finalizar Compra").click()
