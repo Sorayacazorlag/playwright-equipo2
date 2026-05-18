a8199e0c0d874e77fa13a41d04ae23300971944e
Judit <juditcasamitjana@gmail.com>
2026-05-18 14:09:45 +0200

    Fixed menu mobile

--- pages/components/menu.py
+++ pages/components/menu.py
@@ -7,15 +7,31 @@ class Menu:
         self.menu_about_us = "Quiénes Somos"
         self.menu_products = "Productos"
         self.menu_contact = "Contacto"
+        self.ancho = page.viewport_size['width']
 
     def visit_menu_about_us(self):
-        self.page.get_by_role("link", name=self.menu_about_us).click()
+        if self.ancho > 1024:
+            self.page.get_by_role("link", name=self.menu_about_us).click()
+        else:
+            self.page.get_by_role("button", name="Abrir menú principal").click()
+            self.page.get_by_role("menuitem", name=self.menu_about_us).click()
 
-    def visit_menu_products(self):
-        self.page.get_by_role("link", name=self.menu_products).click()
+    def visit_menu_products(self):        
+        if self.ancho > 1024:
+            self.page.get_by_role("link", name=self.menu_products).click()
+        else:
+            self.page.get_by_role("button", name="Abrir menú principal").click()
+            self.page.get_by_role("menuitem", name=self.menu_products).click()
 
-    def visit_menu_contact(self):
-        self.page.get_by_role("link", name=self.menu_contact).click()
+
+    def visit_menu_contact(self):        
+        if self.ancho > 1024:
+            self.page.get_by_role("link", name=self.menu_contact).click()
+        else:
+            self.page.get_by_role("button", name="Abrir menú principal").click()
+            self.page.get_by_role("menuitem", name=self.menu_contact).click()
+        
+
 
     
 
