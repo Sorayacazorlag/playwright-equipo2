007d27f7f35427d116e0c0176438f51fcca4e801
Teresa Mercader <teresam0510@gmail.com>
2026-05-19 06:37:00 +0200

    arreglos

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,16 +1,33 @@
-import re
+
 from playwright.sync_api import Page, expect
 
 class CheckoutPage():
 
     def __init__(self, page: Page):
         self.page = page
-        self.url = "https://web-qa.dev.adalab.es/products"
+        self.url = "https://web-qa.dev.adalab.es/checkout"
 
-    def open_products_page(self):
+    def open_chekcout_page(self):
         self.page.goto(self.url)
 
-    def fill_valid_name(self,name):
-        self.page.get_by_role("searchbox", name="Nombre").fill(name)
+    def fill_complete_name(self,name):
+       self.page.get_by_role("textbox", name="Nombre Completo").fill(name)
 
-  
+    def fill_email(self,email):
+        self.page.get_by_role("textbox", name="Email").fill(email)
+
+    def fill_adress(self,adress):
+        self.page.get_by_role("textbox", name="Dirección").fill(adress)
+
+    def fill_credit_card(self,creditcard):
+        self.page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill(creditcard)
+
+    def complete_payment(self):
+        self.page.get_by_role("button", name="Completar Compra").click()
+
+    def verify_total(self,total):
+        expect(self.page.get_by_text(total)).to_be_visible()
+
+
+
+
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,64 +1,82 @@
+from tabnanny import check
+
 from playwright.sync_api import Page, expect
+
+from pages.cart_page import CartPage
+from pages.confirmation_page import ConfirmationPage
+from pages.products_page import ProductsPage
+from pages.checkout_page import CheckoutPage
 
 
 def test_successful_purchase_with_valid_data(page: Page): #Teresa
 
+    products_page = ProductsPage(page)
+    cart_page = CartPage(page)
+    checkout_page = CheckoutPage(page)
+    confirmation_page = ConfirmationPage(page)
+
     print("Given user open products page")
-    page.goto("https://web-qa.dev.adalab.es/products")
+    products_page.open_products_page()
 
     print ("When user filter by name maceta colgante")
-    page.get_by_role("searchbox", name="Nombre").fill("maceta colgante")
+    products_page.filter_by_name("maceta colgante")
 
     print ("then user add maceta colgante to the cart")
-    page.get_by_role("button", name="Añadir Maceta Colgante al").click()
+    products_page.add_product("Añadir Maceta Colgante")
 
     print ("and the user visit the cart page")
-    page.get_by_role("link", name="Finalizar Compra").click()
+    cart_page.open_cart_page()
 
     print ("verify order sumary prices")
-    page.get_by_label("Resumen del Pedido").get_by_text("14.75 €").click()
-    page.get_by_text("IVA (21%)3.10 €").click()
-    page.get_by_text("5.00 €").click()
-    page.get_by_text("22.85 €").click()
-
+    cart_page.verify_one_product_price("14.75 €")
+    cart_page.verify_vat("IVA (21%)3.10 €")
+    cart_page.verify_shipping("5.00 €")
+    cart_page.verify_total("22.85 €")
+   
     print ("and proceed to payment")
-    page.get_by_role("link", name="Proceder al Pago").click()
+    cart_page.procedd_to_payment()
 
     print ("and fill complete name")
-    page.get_by_role("textbox", name="Nombre Completo").fill("maria garcia")
+    checkout_page.fill_complete_name("maria garcia")
 
     print ("and fill valid email user ")
-    page.get_by_role("textbox", name="Email").fill("test@gmail.com")
-
+    checkout_page.fill_email("test@gmail.com")
+   
     print("and fill valid adress user")
-    page.get_by_role("textbox", name="Dirección").fill("parmenides,5,Malaga")
+    checkout_page.fill_adress("parmenides,5,Malaga")
 
     print ("add valid credit card number")
-    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("4242 4242 4242 4242")
+    checkout_page.fill_credit_card("4242 4242 4242 4242")
 
     print ("complete purchase")
-    page.get_by_role("button", name="Completar Compra").click()
-    expect(page.get_by_role("heading", name="¡Compra Realizada con Éxito!")).to_be_visible()
+    checkout_page.complete_payment()
 
     print("Verify confirmation summary")
+    confirmation_page.verify_confirmation_message()
 
     print("verify products appears")
-    expect(page.get_by_text("Maceta colgante")).to_be_visible()
+    confirmation_page.verify_product_name("Maceta Colgante")
 
     print("verify product price")
-    expect(page.get_by_role("listitem").filter(has_text="Maceta Colgante14.75 €").locator("data")).to_be_visible()
-
+    confirmation_page.verify_product_price("Maceta Colgante14.75 €")
+    
     print("verify product subototal")
-    expect(page.get_by_role("definition").filter(has_text="14.75 €").locator("data")).to_be_visible()
+    confirmation_page.verify_product_subtotal("14.75 €")
 
     print("verify IVA")
-    expect(page.get_by_text("3.10 €")).to_be_visible()
-    expect(page.get_by_text("5.00 €")).to_be_visible()
-    expect(page.get_by_text("22.85 €")).to_be_visible()
+    confirmation_page.verify_IVA("3.10 €")
+    
+    print("verify shipping")
+    confirmation_page.verify_shipping("5.00 €")
 
+    print("verify total price")
+    confirmation_page.verify_total("22.85 €")
+    
     print("back to products page")
-    page.get_by_role("link", name="Volver a la Tienda").click()
-
+    confirmation_page.click_back_to_product()
+    
+    print ("verify products page")
+    products_page.verify_products_url()
 
 
     # ---------------------
